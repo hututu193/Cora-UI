4595301daf11d112cc2b2b5dea4a70044d145382
hututu193 <116326894+hututu193@users.noreply.github.com>
2025-11-11 17:57:32 +0800

    修改Message组件showMessage引用

--- src/components/message-demos/MessageLoading.demo.vue
+++ src/components/message-demos/MessageLoading.demo.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import { showMessage } from '../../lib/message'
+import { showMessage } from '../../lib'
 const showLoading = () => {
   const close = showMessage('正在加载中...', 'loading', 0)
   setTimeout(() => {
